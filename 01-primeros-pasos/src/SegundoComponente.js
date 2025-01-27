410a085386a39144caebe522f832e90d719d67ed
TriMkort <maikol.16.1998@gmail.com>
2025-01-26 20:32:05 -0500

    Se crea segundo componente

---
+++ 01-primeros-pasos/src/SegundoComponente.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SegundoComponente = () => {
+  return (
+    <div className='SegundoComoponente'> {
+
+    }
+      
+    </div>
+  )
+}
+
+export default SegundoComponente
