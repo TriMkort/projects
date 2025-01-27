26d1c2cd8b7bdfcc892eb960a8148e49ffcfd8ad
TriMkort <maikol.stiven.16.1998@gmail.com>
2025-01-27 15:59:46 -0500

    Mi componente
    Creamos el primer componente el cual usaremos para describir los hobbits y de paso lo cargamos en el App.js

---
+++ react/01-primeros-pasos/src/MiComponente.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const MiComponente = () => {
+  return (
+    <div>
+      <h1>Mi componente</h1>
+      <h2>Estos son mi hobbits diarios</h2>
+      <ul>
+        <li>Programar</li>
+        <li>Trabajar</li>
+        <li>Ejercicio</li>
+        <li>Dormir</li>
+      </ul>
+    </div>
+  )
+}
+
+export default MiComponente
