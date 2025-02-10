81c91faef7a29252d8d96f8316efda7dfc7b9098
TriMkort <maikol.stiven.16.1998@gmail.com>
2025-02-09 23:14:45 -0500

    Creamos nuevo create-react-app 02-use-state donde veremos la implementacion de los Hooks y como nos sirven esto en terminos simples son funciones que hacen una interaccion cuando hay un cambio de estado de los componentes

---
+++ react/02-use-state/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h2>Hooks:</h2>
+          <h3>Use-State</h3>
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
