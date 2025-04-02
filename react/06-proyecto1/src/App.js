4a2be6b67bbca6520a4f3878a6d6a0c9c41ca56b
TriMkort <maikol.16.1998@gmail.com>
2025-04-02 02:13:45 -0500

    ajuste en el proyecto eliminacion de archivos

--- react/06-proyecto1/src/App.js
+++ react/06-proyecto1/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div classNameName="layaut">
+            {/*Cabecera*/}
+            <header className="header">
+                <div className="logo">
+                    <div className="play"></div>
+                </div>
+                <h1>Mis-Pelis</h1>
+            </header>
+            {/*Barra de navegacion*/}
+            <nav className="nav">
+                <ul>
+                    <li> <a href="/#">Incio</a></li>
+                    <li> <a href="/#">Peliculas</a></li>
+                    <li> <a href="/#">Blog</a></li>
+                    <li> <a href="/#">Contacto</a></li>
+                </ul>
+            </nav>
+            {/*Contenido principal*/}
+            <section className="content">
+            {/*Aqui iran las peliculas*/}
+                <article className="peli-item">
+                    <h3 className="title">Desarrollo Web</h3>
+                    <p className="Descripción">Descripción</p>
+                    <button className="edit">Editar</button>
+                    <button className="delete">Borrar</button>
+                </article>
+                <article className="peli-item">
+                    <h3 className="title">Desarrollo Web</h3>
+                    <p className="Descripción">Descripción</p>
+                    <button className="edit">Editar</button>
+                    <button className="delete">Borrar</button>
+                </article>
+                <article className="peli-item">
+                    <h3 className="title">Desarrollo Web</h3>
+                    <p className="Descripción">Descripción</p>
+                    <button className="edit">Editar</button>
+                    <button className="delete">Borrar</button>
+                </article>
+                <article className="peli-item">
+                    <h3 className="title">Desarrollo Web</h3>
+                    <p className="Descripción">Descripción</p>
+                    <button className="edit">Editar</button>
+                    <button className="delete">Borrar</button>
+                </article>
+            </section>
+            {/*Barra lateral*/}
+            <aside className="lateral">
+                <div className="search">
+                    <h3 className="title"> Buscador</h3>
+                    <form>
+                        <input type="text"/>
+                        <button>Buscar</button>
+                    </form>
+                </div>
+                <div className="add">
+                    <h3 className="title">Añadir pelicula</h3>
+                    <form>
+                        <input type="text" placeholder="Titulo"/>
+                        <textarea placeholder="Descripción"></textarea>
+                        <input type="submit" value="Guardar"></input>
+                    </form>
+                </div>    
+            </aside>
+            {/*Pie de pagina*/}
+            <footer className="footer">
+                &copy; Máster en JavaScript ES12 y TypeSript <a href="#">victorrobles.es </a>
+            </footer>
+        </div>
+      );
+  }
 
 export default App;
