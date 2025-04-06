f1750dd1afa0168e205ddf535bc2d612d5adb607
TriMkort <maikol.16.1998@gmail.com>
2025-04-06 14:24:48 -0500

    Proyecto 07 Rutas veremos como utilizar la librerita de rutas de react la cual nos facilita y permite navegar por diferentes rutas en nuestros proyectos

---
+++ react/07-rutas/src/componets/Articulo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Articulo = () => {
+    return (
+        <div>
+                <h1>Esta es mi pagina de articulos</h1>
+                <p>Texto principal</p>
+        </div>
+    )
+}
+
+export default Articulo
---
+++ react/07-rutas/src/componets/Contacto.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contacto = () => {
+    return (
+        <div>
+                <h1>Esta es mi pagina de contacto </h1>
+                <p>Texto principal</p>
+        </div>
+    )
+}
+
+export default Contacto
